a1f352d54df0315d71548dc8aed06b9a2436e18f
Robin Thrift <thrift.d.robin@googlemail.com>
2015-04-08 13:39:12 +0200

    chore: release 0.5.2

--- dist/sofaStorages.angular.js
+++ dist/sofaStorages.angular.js
@@ -1,5 +1,5 @@
 /**
- * sofa-storages - v0.5.0 - Tue Apr 07 2015 12:03:13 GMT+0200 (CEST)
+ * sofa-storages - v0.5.2 - Wed Apr 08 2015 13:38:54 GMT+0200 (CEST)
  * http://www.sofa.io
  *
  * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
--- dist/sofaStorages.js
+++ dist/sofaStorages.js
@@ -1,5 +1,5 @@
 /**
- * sofa-storages - v0.5.0 - Tue Apr 07 2015 12:03:13 GMT+0200 (CEST)
+ * sofa-storages - v0.5.2 - Wed Apr 08 2015 13:38:54 GMT+0200 (CEST)
  * http://www.sofa.io
  *
  * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
@@ -7,7 +7,7 @@
  * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
  */
 ;(function (sofa, document, undefined) {
-'use strict';
+/* jshint strict: false */
 /* global sofa */
 /* global store */
 /**
